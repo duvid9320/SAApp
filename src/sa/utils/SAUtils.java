c5d98d9c28f24dd558242a2bc8d3d244f31fe246
duvid9320 <duvid9320@gmail.com>
2017-12-28 03:13:26 -0600

    -Create a method to initialize an editor of JSpinner to MYSQL time format. -Add getters and setters to swing components of ActividadView to use in a controller class.

--- src/sa/utils/SAUtils.java
+++ src/sa/utils/SAUtils.java
@@ -1,7 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 David Rodríguez <duvid9320@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package sa.utils;
 
@@ -12,9 +30,12 @@ import java.io.ObjectOutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
 import sa.model.to.AlumnoTO;
 
 /**
@@ -22,19 +43,26 @@ import sa.model.to.AlumnoTO;
  * @author Dave
  */
 public class SAUtils {
+    private static final String MYSQL_DATE_FORMAT;
+    private static final String MYSQL_TIME_FORMAT;
     
-    public static Date getDateFromMySQLDateString(String date){
+    static{
+        MYSQL_DATE_FORMAT = "YYYY-MM-dd";
+        MYSQL_TIME_FORMAT = "";
+    }
+    
+    public static Date getDateFromString(String date){
         try {
-            return new SimpleDateFormat("YYYY-MM-dd").parse(date);
+            return new SimpleDateFormat(MYSQL_DATE_FORMAT).parse(date);
         } catch (ParseException ex) {
             Logger.getLogger(SAUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
     
-    public static Date getHourDate(String hour){
+    public static Date getTimeFromString(String hour){
         try {
-            return new SimpleDateFormat("HH:mm:ss").parse(hour);
+            return new SimpleDateFormat(MYSQL_TIME_FORMAT).parse(hour);
         } catch (ParseException ex) {
             Logger.getLogger(SAUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -55,6 +83,11 @@ public class SAUtils {
     
     public static boolean stringsAreValid(String... str){
         return Arrays.stream(str).allMatch(s -> isValidString(s));
+    }
+    
+    public static void initSpinnerHourEditor(JSpinner jsp){
+        jsp.setModel(new SpinnerDateModel(new Date(), null, null, Calendar.HOUR_OF_DAY));
+        jsp.setEditor(new JSpinner.DateEditor(jsp, MYSQL_TIME_FORMAT));
     }
     
     public static byte[] getBytesArrayFromObject(Object o){
