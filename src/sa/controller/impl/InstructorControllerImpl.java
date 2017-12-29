aae410a14435670afef00e54842df868b2f23ee7
duvid9320 <duvid9320@gmail.com>
2017-12-29 04:17:00 -0600

    Rebuilding everything alv :v

--- src/sa/controller/impl/InstructorControllerImpl.java
+++ src/sa/controller/impl/InstructorControllerImpl.java
@@ -1,3 +1,26 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 David Rodríguez <duvid9320@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package sa.controller.impl;
 
 
@@ -5,8 +28,9 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTable;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import sa.connection.MySQLManager;
-import sa.controller.InstructorController;
 import sa.model.dao.InstructorDAO;
 import sa.model.to.InstructorTO;
 import sa.utils.SAOutput;
@@ -23,15 +47,28 @@ import sa.view.InstructorView;
  *
  * @author dave
  */
-public class InstructorControllerImpl implements InstructorController{
-    private InstructorView v;
+public class InstructorControllerImpl implements DocumentListener{
+    private final InstructorView view;
+    private final InstructorDAO instructorDAO;
 
-    public InstructorControllerImpl() {
-        v = new InstructorView(this);
-        v.setVisible(true);
+    public InstructorControllerImpl(InstructorView view, InstructorDAO instructorDAO) {
+        this.view = view;
+        this.instructorDAO = instructorDAO;
+        initView();
+        view.setVisible(true);
     }
-
-    @Override
+    
+    private void initView(){
+        addDocumentListener();
+    }
+    
+    private void addDocumentListener(){
+        view.getjTFApellidosInstructor().getDocument().addDocumentListener(this);
+        view.getjTFIdInstructor().getDocument().addDocumentListener(this);
+        view.getjTFNombresInstructor().getDocument().addDocumentListener(this);
+        view.getjTAGradoInstructor().getDocument().addDocumentListener(this);
+    }
+    
     public void createInstructor(InstructorTO instructor) {
         if(!instructor.isValid())
             SAOutput.showErrorMessage("La información del instructor no es valida");
@@ -40,8 +77,7 @@ public class InstructorControllerImpl implements InstructorController{
         else 
             SAOutput.showErrorMessage("El instructor no se pudo registrar");
     }
-
-    @Override
+    
     public void updateInstructor(InstructorTO instructor) {
         if(!instructor.isValid())
             SAOutput.showErrorMessage("La información del instructor no es valida");
@@ -51,7 +87,6 @@ public class InstructorControllerImpl implements InstructorController{
             SAOutput.showErrorMessage("El instructor no se pudo modificar");
     }
 
-    @Override
     public void deleteInstructor(InstructorTO instructor) {
         if(!instructor.isValid())
             SAOutput.showErrorMessage("Carga el instructor desde la tabla");
@@ -61,7 +96,7 @@ public class InstructorControllerImpl implements InstructorController{
             SAOutput.showErrorMessage("El instructor no se pudo eliminar");
     }
 
-    @Override
+    
     public void buscarTodos(JTable jTAResults) {
         try {
             jTAResults.setModel(
@@ -76,9 +111,51 @@ public class InstructorControllerImpl implements InstructorController{
         }
     }
 
-    @Override
     public InstructorTO getInstructor(String id) {
         return id != null ? InstructorDAO.getInstance().getInstructor(id) : null;
+    }
+    
+    private void editIdInstructor(){
+        InstructorTO instructor = instructorDAO.getInstructor(view.getIdInstructor());
+        if(instructor != null)
+            view.setInstructor(instructor);
+        else
+            view.setIdInstructor();
+    }
+    
+    private void enableButtons(){
+        if(!view.getInstructor().isValid())
+            return;
+        InstructorTO instructor = instructorDAO.getInstructor(view.getIdInstructor());
+        view.getjBtnRegistrarInstructor().setEnabled(instructor == null);
+        view.getjBtnModificarInstructor().setEnabled(instructor != null);
+        view.getjBtnEliminarInstructor().setEnabled(instructor != null);
+    }
+    
+    private void textEdited(DocumentEvent e){
+        if(e == view.getjTFApellidosInstructor())
+            view.setApellidosInstructor();
+        else if (e == view.getjTFIdInstructor())
+            editIdInstructor();
+        else if (e == view.getjTFNombresInstructor())
+            view.setNombresInstructor();
+        else if(e == view.getjTAGradoInstructor())
+            view.setGradoInstructor();
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        textEdited(e);
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        textEdited(e);
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        textEdited(e);
     }
     
 }
